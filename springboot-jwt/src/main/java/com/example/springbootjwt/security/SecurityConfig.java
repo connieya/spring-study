5d6bf4734d77a1f04bc0952e7bcccdb0ae8f92c5
gunny6026 <gunny6026@naver.com>
2023-03-24 19:43:53 +0900

    [feat] security filter 설정

---
+++ springboot-jwt/src/main/java/com/example/springbootjwt/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.springbootjwt.security;
+
+import com.example.springbootjwt.filter.CustomAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig   {
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        System.out.println("security !!");
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean(authenticationConfiguration)));
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+}
--- springboot-jwt/src/main/java/com/example/springbootjwt/service/UserServiceImpl.java
+++ springboot-jwt/src/main/java/com/example/springbootjwt/service/UserServiceImpl.java
@@ -6,17 +6,39 @@ import com.example.springbootjwt.repository.RoleRepo;
 import com.example.springbootjwt.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-import static java.rmi.server.LogStream.log;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService , UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("loadUserBy~~  = " + username);
+        User user = userRepo.findByUsername(username);
+        if (user == null){
+            log.error("User not found in database");
+            throw new UsernameNotFoundException("User not found in database");
+        }else {
+            log.info("User found in database : {}",username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),authorities);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -49,4 +71,5 @@ public class UserServiceImpl implements UserService{
         log.info("Fetching all users");
         return userRepo.findAll();
     }
+
 }
