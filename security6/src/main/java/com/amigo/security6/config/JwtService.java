5f0bc5e055d194a74fecd99c1594142474b9df22
gunny6026 <gunny6026@naver.com>
2023-03-26 20:20:42 +0900

    [feat] view 설정

--- security6/src/main/java/com/amigo/security6/config/JwtService.java
+++ security6/src/main/java/com/amigo/security6/config/JwtService.java
@@ -2,11 +2,16 @@ package com.amigo.security6.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,8 +19,25 @@ public class JwtService {
     private static final String SECRET_KEY = "2646294A404D635166546A576E5A7234753778214125442A472D4B6150645267";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token,Claims::getSubject);
     }
+
+    public <T> T extractClaim(String token , Function<Claims , T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(Map<String,Object> extraClaims , UserDetails userDetails){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000 *60 *24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
