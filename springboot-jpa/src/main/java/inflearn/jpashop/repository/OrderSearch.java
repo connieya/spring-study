987fbaf1432887eb8b852678521e52056a74a7ea
gunny6026 <gunny6026@naver.com>
2023-03-31 17:58:43 +0900

    [feat] 회원 등록 /조회

---
+++ springboot-jpa/src/main/java/inflearn/jpashop/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package inflearn.jpashop.repository;
+
+import inflearn.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+}
