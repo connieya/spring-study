440c33914476f56afef22b345219a909ff0de4ae
gunny6026 <gunny6026@naver.com>
2023-04-08 11:31:28 +0900

    [feat] 조회 API

--- springboot-mybatis/src/main/java/study/demo/controller/CompanyController.java
+++ springboot-mybatis/src/main/java/study/demo/controller/CompanyController.java
@@ -2,12 +2,11 @@ package study.demo.controller;
 
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.demo.domain.Company;
 import study.demo.domain.CompanyMapper;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/company")
@@ -19,7 +18,11 @@ public class CompanyController {
 
     @PostMapping("")
     public int post(@RequestBody Company company){
-        System.out.println("company@@@@ = " + company.getName() + company.getAddress());
         return companyMapper.insert(company);
+    }
+
+    @GetMapping("")
+    public List<Company> getAll() {
+        return companyMapper.getAll();
     }
 }
--- springboot-mybatis/src/main/java/study/demo/domain/CompanyMapper.java
+++ springboot-mybatis/src/main/java/study/demo/domain/CompanyMapper.java
@@ -1,10 +1,10 @@
 package study.demo.domain;
 
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Mapper
 public interface CompanyMapper {
@@ -12,4 +12,10 @@ public interface CompanyMapper {
     @Insert("insert into company(company_name , company_address) values(#{company.name}, #{company.address})" )
     int insert(@Param("company") Company company);
 
+    @Select("select * from company")
+    @Results({
+            @Result(property = "name", column = "company_name"),
+            @Result(property = "address", column = "company_address")
+    })
+    List<Company> getAll();
 }
