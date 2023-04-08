cc7ac67602a6916c4209152ea7d6a4a24f25dc82
gunny6026 <gunny6026@naver.com>
2023-04-08 10:44:32 +0900

    [feat] mybatis project starter

---
+++ springboot-mybatis/src/main/java/study/demo/Company.java
@@ -0,0 +1,7 @@
+package study.demo;
+
+public class Company {
+    private  int id;
+    private String name;
+    private String address;
+}
---
+++ springboot-mybatis/src/main/java/study/demo/CompanyMapper.java
@@ -0,0 +1,14 @@
+package study.demo;
+
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface CompanyMapper {
+
+    @Insert("insert into company(company_name , company_address values(#(company.name) #(company.address)" )
+    int insert(@Param("company") Company company);
+
+}
