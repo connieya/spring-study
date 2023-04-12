3e752addb5139502489e6e98451ac4e43f002441
gunny6026 <gunny6026@naver.com>
2023-04-12 15:45:12 +0900

    [feat] 회원 정보 수정 & 이메일 중복 확인

--- springframework-mybatis/src/main/java/com/codingrecipe/study/controller/MemberController.java
+++ springframework-mybatis/src/main/java/com/codingrecipe/study/controller/MemberController.java
@@ -18,45 +18,45 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/save")
-    public String saveForm(){
+    public String saveForm() {
         return "save";
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute MemberDTO memberDTO){
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         int saveResult = memberService.save(memberDTO);
-        if (saveResult > 0){
+        if (saveResult > 0) {
             return "login";
-        }else {
+        } else {
             return "save";
         }
     }
 
     @GetMapping("/login")
-    public String loginForm(){
+    public String loginForm() {
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute MemberDTO memberDTO , HttpSession session) {
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         boolean loignResult = memberService.login(memberDTO);
-        if (loignResult){
-            session.setAttribute("loginEmail",memberDTO.getMemberEmail());
+        if (loignResult) {
+            session.setAttribute("loginEmail", memberDTO.getMemberEmail());
             return "main";
-        }else{
+        } else {
             return "login";
         }
     }
 
     @GetMapping("/")
-    public String findAll(Model model){
-        List<MemberDTO> memberDTOList =  memberService.findAll();
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
         model.addAttribute("memberList", memberDTOList);
         return "list";
     }
 
     @GetMapping("")
-    public String findById(@RequestParam("id") Long id , Model model){
+    public String findById(@RequestParam("id") Long id, Model model) {
         MemberDTO memberDTO = memberService.findById(id);
         model.addAttribute("member", memberDTO);
         return "detail";
@@ -66,6 +66,31 @@ public class MemberController {
     public String delete(@RequestParam("id") Long id) {
         memberService.delete(id);
         return "redirect:/member/";
+    }
+
+    @GetMapping("/update")
+    public String updateForm(HttpSession session, Model model) {
+        String loginEmail = (String) session.getAttribute("loginEmail");
+        System.out.println("loginEmail = " + loginEmail);
+        MemberDTO memberDTO = memberService.findByMemberEmail(loginEmail);
+        model.addAttribute("member", memberDTO);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute MemberDTO memberDTO){
+        boolean result = memberService.update(memberDTO);
+        if (result){
+            return "redirect:/member?id="+memberDTO.getId();
+        }
+        return "index";
+
+    }
+
+    @PostMapping("/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail){
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
     }
 
 
--- springframework-mybatis/src/main/java/com/codingrecipe/study/service/MemberService.java
+++ springframework-mybatis/src/main/java/com/codingrecipe/study/service/MemberService.java
@@ -37,4 +37,25 @@ public class MemberService {
     public void delete(Long id) {
         memberRepository.delete(id);
     }
+
+    public MemberDTO findByMemberEmail(String loginEmail) {
+        return memberRepository.findByMemberEmail(loginEmail);
+    }
+
+    public boolean update(MemberDTO memberDTO) {
+        int result = memberRepository.update(memberDTO);
+        if (result > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String emailCheck(String memberEmail) {
+        MemberDTO dto = memberRepository.findByMemberEmail(memberEmail);
+        if (dto == null) {
+            return "ok";
+        }
+        return "no";
+    }
 }
