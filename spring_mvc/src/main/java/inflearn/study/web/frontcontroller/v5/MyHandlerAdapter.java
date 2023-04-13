370ebb2fea04710faf872f0ae6885d183a7a0218
gunny6026 <gunny6026@naver.com>
2023-04-13 09:17:01 +0900

    [feat] Front Controller V5

---
+++ spring_mvc/src/main/java/inflearn/study/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,17 @@
+package inflearn.study.web.frontcontroller.v5;
+
+import inflearn.study.web.frontcontroller.ModelView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request , HttpServletResponse response, Object handler) throws ServletException , IOException;
+
+
+}
