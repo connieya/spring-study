4605c1521ae3a8482d7af93ecf69d5e77ab94da1
gunny6026 <gunny6026@naver.com>
2023-04-14 09:31:14 +0900

    [feat] 게시글 등록 ,조회, 삭제

--- spring_mvc/src/main/java/inflearn/study/web/springmvc/v3/SpringMemberControllerV3.java
+++ spring_mvc/src/main/java/inflearn/study/web/springmvc/v3/SpringMemberControllerV3.java
@@ -2,14 +2,11 @@ package inflearn.study.web.springmvc.v3;
 
 import inflearn.study.domain.member.Member;
 import inflearn.study.domain.member.MemberRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
---
+++ springframework-mybatis-board/src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,53 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        int saveResult = boardService.save(boardDTO);
+        if (saveResult > 0) {
+            return "redirect:/board/";
+        }
+        return "save";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<BoardDTO> boardList = boardService.findAll();
+        model.addAttribute("boardList", boardList);
+        return "list";
+    }
+
+    @GetMapping
+    public String findById(@RequestParam("id") Long id , Model model ) {
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board",boardDTO);
+        return "detail";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") Long id){
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
+}
