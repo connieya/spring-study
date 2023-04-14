34da06216e46828458ab95fd26d54b5d5bf73058
gunny6026 <gunny6026@naver.com>
2023-04-14 14:54:37 +0900

    [feat] 게시글 페이징

--- springframework-mybatis-board/src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ springframework-mybatis-board/src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.codingrecipe.board.controller;
 
 import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.dto.PageDTO;
 import com.codingrecipe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -25,7 +26,7 @@ public class BoardController {
     public String save(@ModelAttribute BoardDTO boardDTO) {
         int saveResult = boardService.save(boardDTO);
         if (saveResult > 0) {
-            return "redirect:/board/";
+            return "redirect:/board/paging";
         }
         return "save";
     }
@@ -38,16 +39,44 @@ public class BoardController {
     }
 
     @GetMapping
-    public String findById(@RequestParam("id") Long id , Model model ) {
+    public String findById(@RequestParam("id") Long id,
+                           @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                           Model model) {
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
-        model.addAttribute("board",boardDTO);
+        model.addAttribute("board", boardDTO);
+        model.addAttribute("page",page);
         return "detail";
     }
 
     @GetMapping("/delete")
-    public String delete(@RequestParam("id") Long id){
+    public String delete(@RequestParam("id") Long id) {
         boardService.delete(id);
         return "redirect:/board/";
+    }
+
+    @GetMapping("/update")
+    public String updateForm(@RequestParam("id") Long id, Model model) {
+        BoardDTO dto = boardService.findById(id);
+        model.addAttribute("board", dto);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
+        boardService.update(boardDTO);
+        BoardDTO dto = boardService.findById(boardDTO.getId());
+        model.addAttribute("board", dto);
+        return "detail";
+//        return "redirect:/board?id="+dto.getId();
+    }
+
+    @GetMapping("/paging")
+    public String paging(Model model, @RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+        List<BoardDTO> boardDTOList = boardService.pagingList(page);
+        PageDTO pageDTO = boardService.pagingParam(page);
+        model.addAttribute("boardList", boardDTOList);
+        model.addAttribute("paging", pageDTO);
+        return "paging";
     }
 }
--- springframework-mybatis-board/src/main/java/com/codingrecipe/board/repository/BoardRepository.java
+++ springframework-mybatis-board/src/main/java/com/codingrecipe/board/repository/BoardRepository.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
@@ -31,5 +32,17 @@ public class BoardRepository {
 
     public void delete(Long id) {
         sql.delete("Board.delete",id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        sql.update("Board.update",boardDTO);
+    }
+
+    public List<BoardDTO> pagingList(Map<String, Integer> pagingParams) {
+        return sql.selectList("Board.paging", pagingParams);
+    }
+
+    public int boardCount() {
+        return sql.selectOne("Board.boardCount");
     }
 }
